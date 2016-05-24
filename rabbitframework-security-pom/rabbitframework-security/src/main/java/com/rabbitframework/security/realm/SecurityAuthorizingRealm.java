54e2b58190a67d941061989964892210a71777d9
liangjy <28554768@qq.com>
2016-05-24 14:08:02 +0800

    update

--- rabbitframework-security-pom/rabbitframework-security/src/main/java/com/rabbitframework/security/realm/SecurityAuthorizingRealm.java
+++ rabbitframework-security-pom/rabbitframework-security/src/main/java/com/rabbitframework/security/realm/SecurityAuthorizingRealm.java
@@ -14,45 +14,47 @@ import org.apache.shiro.util.StringUtils;
  * @date: 16/5/20 下午1:52
  */
 public abstract class SecurityAuthorizingRealm extends AuthorizingRealm {
-    private static final String DEFAULT_CACHE_KEY_PREFIX = "security_realm_key:";
-    private String cacheKeyPrefix = DEFAULT_CACHE_KEY_PREFIX;
-    private String authc_key = "authc:";
-    private String authz_key = "authz:";
+	private static final String DEFAULT_CACHE_KEY_PREFIX = "security_realm_key:";
+	private String cacheKeyPrefix = DEFAULT_CACHE_KEY_PREFIX;
+	private String authc_key = "authc:";
+	private String authz_key = "authz:";
 
-    @Override
-    protected Object getAuthenticationCacheKey(PrincipalCollection principals) {
-        String prefix = cacheKeyPrefix + authc_key;
-        return getCacheKey(prefix, principals);
-    }
+	@Override
+	protected Object getAuthenticationCacheKey(PrincipalCollection principals) {
+		String prefix = cacheKeyPrefix + authc_key;
+		return getCacheKey(prefix, principals);
+	}
 
-    protected Object getCacheKey(String prefix, PrincipalCollection principals) {
-        Object userObject = getAvailablePrincipal(principals);
-        if (userObject == null) {
-            return principals;
-        }
+	protected Object getCacheKey(String prefix, PrincipalCollection principals) {
+		Object userObject = getAvailablePrincipal(principals);
+		if (userObject == null) {
+			return principals;
+		}
 
-        String userId = null;
-        if (userObject instanceof SecurityUser) {
-            SecurityUser shiroUser = (SecurityUser) userObject;
-            userId = shiroUser.getUserId();
-        }
+		String userId = null;
+		if (userObject instanceof SecurityUser) {
+			SecurityUser shiroUser = (SecurityUser) userObject;
+			userId = shiroUser.getUserId();
+		}
 
-        if (StringUtils.hasLength(userId)) {
-            return prefix + userId;
-        }
+		if (StringUtils.hasLength(userId)) {
+			return prefix + userId;
+		}
 
-        return principals;
-    }
+		return principals;
+	}
 
-    @Override
-    protected Object getAuthorizationCacheKey(PrincipalCollection principals) {
-        String prefix = cacheKeyPrefix + authz_key;
-        return getCacheKey(prefix, principals);
-    }
+	@Override
+	protected Object getAuthorizationCacheKey(PrincipalCollection principals) {
+		String prefix = cacheKeyPrefix + authz_key;
+		return getCacheKey(prefix, principals);
+	}
 
-    public void cleanAuthorizationCache(String userId) {
-        String prefix = cacheKeyPrefix + authz_key + userId;
-        Cache<Object, AuthorizationInfo> cache = getAuthorizationCache();
-        cache.remove(prefix);
-    }
+	public void cleanAuthorizationCache(String userId) {
+		String prefix = cacheKeyPrefix + authz_key + userId;
+		Cache<Object, AuthorizationInfo> cache = getAuthorizationCache();
+		if (cache != null) {
+			cache.remove(prefix);
+		}
+	}
 }
