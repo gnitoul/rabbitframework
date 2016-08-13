88f887787d3c74919472a68bb223f4b4bde68ee5
justinliang <28554768@qq.com>
2016-08-14 00:37:45 +0800

    update

--- rabbitframework-web/src/main/java/com/rabbitframework/web/servlet/RabbitServletContainer.java
+++ rabbitframework-web/src/main/java/com/rabbitframework/web/servlet/RabbitServletContainer.java
@@ -1,5 +1,14 @@
 package com.rabbitframework.web.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletContainer;
 
@@ -16,9 +25,21 @@ public class RabbitServletContainer extends ServletContainer {
 	/**
 	 * Create Jersey Servlet container.
 	 *
-	 * @param resourceConfig container configuration.
+	 * @param resourceConfig
+	 *            container configuration.
 	 */
 	public RabbitServletContainer(final ResourceConfig resourceConfig) {
 		super(resourceConfig);
+	}
+
+	@Override
+	public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		super.doFilter(request, response, chain);
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		super.service(req, res);
 	}
 }
