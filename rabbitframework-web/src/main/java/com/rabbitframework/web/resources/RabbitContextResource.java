4516e539be24ecbced075efa7825817fcc226b89
justinliang <28554768@qq.com>
2016-08-15 20:28:44 +0800

    update

--- rabbitframework-web/src/main/java/com/rabbitframework/web/resources/RabbitContextResource.java
+++ rabbitframework-web/src/main/java/com/rabbitframework/web/resources/RabbitContextResource.java
@@ -1,38 +1,109 @@
 package com.rabbitframework.web.resources;
+import java.net.URLEncoder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.ws.rs.container.ResourceContext;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.Request;
-import javax.ws.rs.core.SecurityContext;
-import javax.ws.rs.core.UriInfo;
-import javax.xml.ws.spi.http.HttpContext;
+import javax.servlet.http.HttpSession;
 
-import org.glassfish.jersey.server.CloseableService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.rabbitframework.web.exceptions.ResourceException;
 
 public abstract class RabbitContextResource {
-	@Context
-	protected UriInfo uriInfo;
+	private static final Logger logger = LoggerFactory.getLogger(RabbitContextResource.class);
 
-	@Context
-	protected Request restRequest;
+	/**
+	 * 获取当前url路径
+	 * 
+	 * @return
+	 * @throws Exception
+	 */
+	protected String getCurrentUrl(HttpServletRequest request) throws ResourceException {
+		String urlBase = request.getRequestURL().toString();
+		String urlParameters = request.getQueryString();
+		try {
+			return URLEncoder.encode(urlBase + "?" + urlParameters, "UTF-8");
+		} catch (Exception e) {
+			logger.error(e.getMessage(), e);
+			throw new ResourceException(e.getMessage(), e);
+		}
+	}
 
-	@Context
-	protected SecurityContext securityContext;
+	/**
+	 * 根据参数获取session中的信息
+	 * 
+	 * @param att
+	 * @return
+	 */
+	protected Object getSessionAttValue(HttpServletRequest request, String att) {
+		return getHttpSession(request).getAttribute(att);
+	}
 
-	@Context
-	protected HttpContext httpContext;
+	/**
+	 * 根据参数设置上下文类型
+	 * 
+	 * @param type
+	 */
+	protected void setContentType(HttpServletResponse response, String type) {
+		response.setContentType(type);
+	}
 
-	@Context
-	protected CloseableService closeableService;
+	/**
+	 * 设置上下文默认类型 默认为setContentType("text/json; charset=utf-8");
+	 */
+	protected void setContentType(HttpServletResponse response) {
+		setContentType(response, "text/json; charset=utf-8");
+	}
 
-	@Context
-	protected HttpServletRequest request;
+	/**
+	 * 获取当前工程名路径
+	 * 
+	 * @return
+	 */
+	protected String getURLBase(HttpServletRequest request) {
+		return request.getContextPath();
+	}
 
-	@Context
-	protected HttpServletResponse response;
+	/**
+	 * 获取当前工程详细路径
+	 * 
+	 * @return
+	 */
+	protected String getRealPath(HttpServletRequest request) {
+		return request.getServletContext().getRealPath("/");
+	}
 
-	@Context
-	protected ResourceContext resourceContext;
+	/**
+	 * 获取session
+	 * 
+	 * @return
+	 */
+	protected final HttpSession getHttpSession(HttpServletRequest request) {
+		return request.getSession();
+	}
+
+	// @Context
+	// protected UriInfo uriInfo;
+	//
+	// @Context
+	// protected Request restRequest;
+	//
+	// @Context
+	// protected SecurityContext securityContext;
+	//
+	// @Context
+	// protected HttpContext httpContext;
+	//
+	// @Context
+	// protected CloseableService closeableService;
+	//
+	// @Context
+	// protected HttpServletRequest request;
+	//
+	// @Context
+	// protected HttpServletResponse response;
+	//
+	// @Context
+	// protected ResourceContext resourceContext;
 }
